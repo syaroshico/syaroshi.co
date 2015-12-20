0c4ab85b4d9b577b9be5a390b062ca2453740e7a
sudosan <admin@syaroshi.co>
2015-12-21 05:37:43 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+  <title>シャロシコしよう！</title>
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+  <div class="container-fluid">
+  	<div class="row">
+  	  <div class="col-xs-12 col-sm-10 col-md-8 col-lg-6 col-sm-offset-1 col-md-offset-2 col-lg-offset-3">
+            <div class="content">
+              <div class="col-xs-12 col-sm-8 col-sm-offset-2">
+                <img src="img/logo.png" class="img-responsive" alt="シャロでしこりますか？">
+              </div>
+            </div>
+          </div>
+          <div class="col-xs-12 col-sm-10 col-md-8 col-lg-6 col-sm-offset-1 col-md-offset-2 col-lg-offset-3">
+            <div class="content">
+              <h1>Let's シャロシコ！シャロシコStep</h1>
+                <div class="category">
+                  <h2>1,シャロを探す</h2>
+                  <hr>
+                  <div class="col-xs-12 col-sm-4 center"><a class="button pixiv" href="http://www.pixiv.net/search.php?word=%E6%A1%90%E9%96%93%E7%B4%97%E8%B7%AF" target="_blank">Pixiv</a></div>
+                  <div class="col-xs-12 col-sm-4 center"><a class="button nijie" href="http://nijie.info/search.php?word=%E6%A1%90%E9%96%93%E7%B4%97%E8%B7%AF" target="_blank">ニジエ</a></div>
+                  <div class="col-xs-12 col-sm-4 center"><a class="button merobu" href="https://www.melonbooks.co.jp/search/search.php?mode=search&search_disp=&chara=&orderby=&disp_number=100&pageno=1&text_type=all&name=%E3%82%B7%E3%83%A3%E3%83%AD&is_end_of_sale2=1&category_ids%5B%5D=1&additional%5B%5D=r18&genre=0&co_name=&ci_name=&sale_date_before=&sale_date_after=&price_low=0&price_high=0&disp_number=100" target="_blank">メロンブックス</a></div>
+                  <br>
+                </div>
+              <div class="category">
+                <h2>2,致す</h2>
+                <hr>
+                <div class="col-xs-12 col-sm-4 center"><a class="button tenga" href="http://amzn.to/1OtobCe" target="_blank">TENGA</a></div>
+                <div class="col-xs-12 col-sm-4 center"><a class="button lotion" href="http://amzn.to/1RBMagz" target="_blank">ローション</a></div>
+                <div class="col-xs-12 col-sm-4 center"><a class="button cd" href="http://amzn.to/1Otp3XM" target="_blank">CD</a></div>
+                <br>
+              </div>
+              <div class="category">
+                <h2>3,報告</h2>
+                <hr>
+                <div class="col-xs-12 col-sm-4 center"><a class="button tweet" href="http://twitter.com/share?text=%E3%82%B7%E3%83%A3%E3%83%AD%E3%82%B7%E3%82%B3%E3%81%97%E3%81%BE%E3%81%97%E3%81%9F%EF%BC%81&url=http://syaroshi.co/&hashtags=%E3%82%B7%E3%83%A3%E3%83%AD%E3%82%B7%E3%82%B3" target="_blank">ツイート</a></div>
+                <div class="col-xs-12 col-sm-4 center"><a class="button facebook" href="http://www.facebook.com/sharer.php?u=http://syaroshi.co/index.php&amp;t=%E3%82%B7%E3%83%A3%E3%83%AD%E3%82%B7%E3%82%B3%E3%81%97%E3%81%BE%E3%81%97%E3%81%9F%EF%BC%81" target="_blank">Facebookでシェア</a></div>
+                <div class="col-xs-12 col-sm-4 center"><a class="button line" href="http://line.naver.jp/R/msg/text/?%e3%82%b7%e3%83%a3%e3%83%ad%e3%82%b7%e3%82%b3%e3%81%97%e3%81%be%e3%81%97%e3%81%9f%ef%bc%81%0d%0ahttp%3a%2f%2fsyaroshi%2eco" target="_blank">LINEで送る</a></div>  
+                <br>
+              </div>
+            </div>
+          </div>
+          <div class="col-xs-12 col-sm-10 col-md-8 col-lg-6 col-sm-offset-1 col-md-offset-2 col-lg-offset-3">
+          <footer>
+            <div class="content">
+              <div class="col-xs-12 col-sm-5">
+                <a class="twitter-timeline" href="https://twitter.com/search?q=%E3%82%B7%E3%83%A3%E3%83%AD%E3%82%B7%E3%82%B3" data-widget-id="666239757488287750">シャロシコに関するツイート</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+              </div>
+              <div class="col-xs-12 col-sm-7">
+                 <h3>シャロシコツイート数</h3>
+                 <h4 style="color:red;">祝！<a style="color:#8c0000;" href="https://api.syaroshi.co" target="_blank">シャロシコAPI</a>稼働！</h4>
+                 <?php
+        	// チェックするURL
+//        	$url = 'http://syaroshi.co/' ;
+//     		 $url2 = 'https://syaroshi.co/' ;
+		// JSONデータを取得
+		$json = @file_get_contents( 'https://api.syaroshi.co/api/v1/count.json' ) ;
+//	        $json2 = @file_get_contents( 'http://urls.api.twitter.com/1/urls/count.json?url=' . rawurlencode( $url2 ) ) ;
+		// JSONデータを配列に直す
+		$array = json_decode( $json , true ) ;
+//	        $array2 = json_decode( $json2 , true ) ;
+		// カウント
+		$count = $array[ 'count' ] ;
+//	        $count2 = $array2[ 'count' ] ;
+//     		 $countall = $count + $count2 ;
+//		echo "<p>クソツイッターがAPIを廃止したため表示できません</p>";
+		echo  '<p>' . number_format( $count ) . 'ツイート</p>'; 
+		?>
+                 
+                 <h3>作った人</h3>
+                 <p><a href="http://fleurdulap.in/" target="_blank">桐間紗路</a>,<a href="https://twitter.com/sudosan" target="_blank">@sudosan</a>,<a href="https://twitter.com/hnle0" target="_blank">@hnle0</a></p>
+                 <h3>メールアドレス</h3>
+                 <a href="mailto:kirima@syaroshi.co">kirima@syaroshi.co</a>
+                 <h3>広告</h3>
+	         <a href="http://www.cross-law.jp/" target="_blank"><img src="img/cross.png" width="350" height="250" class="img-responsive"></a>
+
+              </div>
+            </div>
+          </footer>
+        </div>
+    </div>
+  </div>
+<!--Googleアナリティクス-->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-64576587-3', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</body>
+</html>
